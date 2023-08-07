babab210f0fe0ac559bb67fc19cd7fc745e487a7
Kamil Holody <kamilholody@live.com>
2023-08-07 15:43:11 +0200

    Page Objects Pattern - Feedback Test with Fixtures

---
+++ cypress/integration/e2e/feedback.spec.js
@@ -0,0 +1,12 @@
+import FeedbackPage from "../../page-objects/pages/FeedbackPage"
+
+describe('Feedback Test using Fixtures', () => {
+    
+    before(function() {
+        FeedbackPage.load()
+    })
+
+    it('should submit feedback form', () => {
+        FeedbackPage.submitFeedback()
+    })
+})
