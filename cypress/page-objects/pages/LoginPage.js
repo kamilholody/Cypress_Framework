bbfe4d80444c7a6ffc9aab72ebf66831d8a2743c
Kamil Holody <kamilholody@live.com>
2023-08-07 12:16:29 +0200

    Page Objects Pattern - Pages

---
+++ cypress/page-objects/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import BasePage from "../Base-Page"
+
+export default class LoginPage extends BasePage{
+    static login(username, password) {
+        cy.login(username, password)
+    }
+
+    static clickForgotPasswordLink() {
+        cy.contains('Forgot your password ?').click()
+    }
+
+    static displayErrorMessage() {
+        //cy.get('.alert-error').should('be.visible')
+        cy.isVisible('.alert-error')
+    }
+}
